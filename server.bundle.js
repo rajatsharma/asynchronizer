cd72c9c71669d662152699d3bb7671e8467a24cd
Rajat Sharma <lunasunkaiser@gmail.com>
2017-10-28 23:07:39 +0530

    Updates

---
+++ server.bundle.js
@@ -0,0 +1,152 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 5);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	module.exports = require("express");
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	"use strict";
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var config = {
+	  port: process.env.PORT || 8000
+	};
+	
+	exports.default = config;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _express = __webpack_require__(0);
+	
+	var router = new _express.Router();
+	
+	router.route('/').get(function (req, res) {
+	  return res.json('Server Working Fine');
+	});
+	
+	exports.default = router;
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports = require("body-parser");
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	module.exports = require("compression");
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _express = __webpack_require__(0);
+	
+	var _express2 = _interopRequireDefault(_express);
+	
+	var _compression = __webpack_require__(4);
+	
+	var _compression2 = _interopRequireDefault(_compression);
+	
+	var _bodyParser = __webpack_require__(3);
+	
+	var _bodyParser2 = _interopRequireDefault(_bodyParser);
+	
+	var _ping = __webpack_require__(2);
+	
+	var _ping2 = _interopRequireDefault(_ping);
+	
+	var _config = __webpack_require__(1);
+	
+	var _config2 = _interopRequireDefault(_config);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	// Initialize the Express App
+	var app = new _express2.default();
+	
+	// Apply body Parser and server public assets and routes
+	app.use((0, _compression2.default)());
+	app.use(_bodyParser2.default.json({ limit: '20mb' }));
+	app.use(_bodyParser2.default.urlencoded({ limit: '20mb', extended: false }));
+	app.use('/', _ping2.default);
+	
+	// start app
+	app.listen(_config2.default.port, function (error) {
+	  if (error) {
+	    console.log('Something Went Wrong');
+	    return;
+	  }
+	  console.log('Server running at ' + _config2.default.port);
+	});
+	
+	exports.default = app;
+
+/***/ }
+/******/ ]);
