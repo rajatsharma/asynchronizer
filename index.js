95295ae4fcd5539246bcbaa5a111a88a6cbdf187
Rajat Sharma <lunasunkaiser@gmail.com>
2017-11-04 20:20:16 +0530

    Updates

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Entry Script
+ */
+
+if (process.env.NODE_ENV === 'production') {
+  // In production, serve the webpacked server file.
+  require('./server.bundle.js')
+} else {
+  // Babel polyfill to convert ES6 code in runtime
+  require('babel-register')
+  require('babel-polyfill')
+  require('./src/server')
+}
